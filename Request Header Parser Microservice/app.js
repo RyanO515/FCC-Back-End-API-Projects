a9fc59a90aa2433a91ca688f2ef43e81e57694c6
Ryan <ROliver@zghealth.com>
2016-04-05 12:19:57 -0400

    initial commit -client header parser

---
+++ Request Header Parser Microservice/app.js
@@ -0,0 +1,35 @@
+var express    = require("express"),
+	bodyParser = require("body-parser"),
+	PORT       = process.env.PORT || 3000,
+	getIP      = require('ipware')().get_ip,
+	app        = express();
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.get("/", function (req, res) {
+	var ip = getIP(req).clientIp;
+	
+    var info = {
+         'ip-address': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+    };
+    
+    res.send(info);
+});
+
+// more ip and header info used in this app can be found at:
+// http://stackoverflow.com/questions/19266329/node-js-get-clients-ip
+// http://stackoverflow.com/questions/8107856/how-to-determine-a-users-ip-address-in-node/26310355#26310355
+
+
+
+
+
+app.listen(PORT, function (req, res) {
+	console.log("Server is listening!!!");
+});
