22a6b4f4aad36de03d12d82e42ad4739e4836f58
Ryan <ROliver@zghealth.com>
2016-04-26 15:44:37 -0400

    completed URLittle comments and link styling

--- URLittle/app.js
+++ URLittle/app.js
@@ -15,23 +15,28 @@ mongoose.connect(process.env.DATABASE_URL, function (err) {
 	else console.log("connected to database" + process.env.DATABASE_URL);
 });
 
+// INDEX view
 app.get("/", function (req, res) {
 	res.render("index");
 });
 
+// NEW URL view
 app.get("/new/:url", function (req, res) {
 	var url = req.params.url;
 	var id = Math.floor(Math.random() * 10000) + "1";
 
+	// set up new link from schema
 	var link = new Links({
 		url: url,
 		id: id
 	});
 
+	//save to collection
 	link.save(function (err) {
 		if (err) console.log(err);
 	});
 
+	// construct object to send to screen
 	var jsonLink = {
 		"original_url": "http://" + url,
 		"short_url": "https://urlittle.herokuapp.com/short/ened/" + id
@@ -40,10 +45,13 @@ app.get("/new/:url", function (req, res) {
 	res.send(jsonLink);
 });
 
+
+//shortened URL view
 app.get("/short/ened/:id", function (req, res) {
 
 	console.log(req.params.id);
 	Links.find({
+		// get link from url
 		id: req.params.id
 	}, function (err, link) {
 
@@ -52,9 +60,11 @@ app.get("/short/ened/:id", function (req, res) {
 		else {
 			console.log(req.params.id);
 			console.log(link);
+			// first url in db
 			var getLink = link;
 			var goToUrl = getLink[0].url;
 
+			// sends to website
 			res.redirect("http://" + goToUrl);
 		}
 	});
